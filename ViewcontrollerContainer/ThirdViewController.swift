aaac2217320f93f18d7b329c69ec674424c9fa85
Habeeb <habeeb@pepul.com>
2022-09-22 16:03:58 +0530

    4:03pm

---
+++ ViewcontrollerContainer/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  ViewcontrollerContainer
+//
+//  Created by Habeeb Mohamed on 22/09/22.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ViewcontrollerContainer/ViewController.swift
+++ ViewcontrollerContainer/ViewController.swift
@@ -8,12 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var secondviewcontroller = SecondViewController()
+    var thirdviewccontroller = ThirdViewController()
+    var alertcontroller = UIAlertController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+        
     }
-
-
+    
+    
+    func setupView() {
+        
+        let height = self.view.frame.height
+        
+        alertcontroller = UIAlertController(title: "Enter some message", message: "", preferredStyle: .alert)
+        
+        alertcontroller.addTextField { textfileds in
+            
+            textfileds.placeholder = "Enter your value"
+            
+        }
+        
+        addChild(secondviewcontroller)
+        addChild(thirdviewccontroller)
+        addChild(alertcontroller)
+        
+        self.view.addSubview(secondviewcontroller.view)
+        self.view.addSubview(thirdviewccontroller.view)
+        self.view.addSubview(alertcontroller.view)
+        
+        secondviewcontroller.didMove(toParent: self)
+        thirdviewccontroller.didMove(toParent: self)
+        alertcontroller.didMove(toParent: self)
+        
+        secondviewcontroller.view.frame = CGRect(x: 0, y: 0, width: 80, height: height)
+        thirdviewccontroller.view.frame = CGRect(x: 0, y: 0, width: 80, height: height)
+        alertcontroller.view.frame = CGRect(x: 100, y: 100, width: 100, height: 200)
+        
+        thirdviewccontroller.view.isHidden = true
+        alertcontroller.view.isHidden = true
+        
+    }
+    
+    
+    // MARK: -
+    
+    @IBAction func tapOnSegment(segment: UISegmentedControl) {
+        
+        secondviewcontroller.view.isHidden = true
+        thirdviewccontroller.view.isHidden = true
+        alertcontroller.view.isHidden = true
+        
+        let index = segment.selectedSegmentIndex
+        
+        if index == 0 {
+            
+            secondviewcontroller.view.isHidden = false
+            
+        } else if index == 1 {
+            
+            thirdviewccontroller.view.isHidden = false
+            
+        } else if index == 2 {
+            
+            let storyboardRef = UIStoryboard(name: "segueBoard", bundle: nil)
+            
+            let newVC = storyboardRef.instantiateViewController(withIdentifier: "segueview") as! SegueViewController
+            
+            navigationController?.pushViewController(newVC, animated: true)
+            
+            // alertcontroller.view.isHidden = false
+            
+        }
+    }
+    
 }
 
